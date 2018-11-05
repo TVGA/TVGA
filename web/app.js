73ff95f01d0f254331e2e229a69372cb29262e8d
xxnonameboyzz <imthebestfcktherest@gmail.com>
2018-11-05 16:33:24 +0000

    .

---
+++ web/app.js
@@ -0,0 +1,63 @@
+// Dependencies
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const express = require('express');
+const constants = require('constants');
+const helmet = require('helmet')
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+
+let indexRouter = require('./routes/index');
+
+let app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/tvga.ml/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/tvga.ml/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/tvga.ml/chain.pem', 'utf8');
+const dhparam =  fs.readFileSync("/etc/letsencrypt/archive/tvga.ml/dh4096.pem");
+
+const credentials = {
+    secureOptions: constants.SSL_OP_NO_SSLv3 | constants.SSL_OP_NO_SSLv2,
+	key: privateKey,
+	cert: certificate,
+    ca: ca,
+    dhparam: dhparam
+};
+
+app.use(helmet.hsts({
+    maxAge: 31536000000,
+    includeSubdomains: true,
+    force: true
+}));
+
+app.use('/', indexRouter);
+
+// Starting both http & https servers
+const httpServer = http.createServer(function (req, res) {
+    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    res.end();
+});
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
---
+++ web/streams/streams.js
@@ -0,0 +1,35 @@
+let streamLogin = require('./streamLogin.json');
+
+let canais = [
+    {
+        'id': '7049',
+        'nome': 'sporttv1'
+    },
+    {
+        'id': '29089',
+        'nome': 'elevensports1'
+    }
+]
+
+let commands = []
+
+Object.keys(streamLogin).forEach(key => {
+    let login = streamLogin[key];
+
+    let username = login.username;
+    let password = login.password;
+
+    let url = 'http://bestbuyiptv.link:6969/live/' + username + '/' + password + '/' + canais[key].id + '.ts';
+
+    let command = 'ffmpeg -i ' + url + ' -c:v copy -hls_time 2 -hls_list_size 2 -hls_flags delete_segments -f hls ../public/stream/' + canais[key].nome + '.m3u8';
+
+    commands.push(command);
+});
+
+let dataCallback = function(data) {
+    console.log(data);
+};
+
+let options = { mode: 'parallel', onData: dataCallback, verbose: true };
+
+nrc.run(commands, options);
