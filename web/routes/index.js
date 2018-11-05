2c692ae1945b4c62c778f5621ed89bb78c5678f7
xxnonameboyzz <imthebestfcktherest@gmail.com>
2018-11-05 16:52:54 +0000

    .

--- web/routes/index.js
+++ web/routes/index.js
@@ -5,4 +5,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'TVGA'});
 });
 
+router.get('/sporttv1', function(req, res, next) {
+  res.render('stream', { title: 'TVGA > SPORT TV 1', file: '/stream/sporttv1.m3u8', poster: 'https://i0.wp.com/gigatuga.net/wp-content/uploads/2017/09/spottv-1HD.png?fit=800%2C450'});
+});
+
 module.exports = router;
