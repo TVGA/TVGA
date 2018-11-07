b468198a852cfbad81e1c5dceffd677d21be8f0b
xxnonameboyzz <imthebestfcktherest@gmail.com>
2018-11-07 21:39:25 +0000

    .

--- web/app.js
+++ web/app.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(subdomain('admin', adminRouter));
+app.use(subdomain('admin', adminApp));
 app.use('/', indexRouter);
 
 // Certificate
