f5fecfcd62b5512fcdc3e32b3483b835cb6fb131
xxnonameboyzz <imthebestfcktherest@gmail.com>
2018-11-07 20:05:47 +0000

    .

--- web/admin/routes/admin.js
+++ web/admin/routes/admin.js
@@ -1,8 +1,14 @@
 let express = require('express');
 let router = express.Router();
 
+let app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 router.get('/', function(req, res) {
-    res.send('Admin Panel');
+    res.render('index');
 });
 
 module.exports = router;
