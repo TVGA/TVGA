8fe07ed47bf293edb69b0064c82d78cda5a21065
xxnonameboyzz <imthebestfcktherest@gmail.com>
2018-11-07 21:02:20 +0000

    .

--- web/admin/routes/admin.js
+++ web/admin/routes/admin.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 let path = require('path');
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
