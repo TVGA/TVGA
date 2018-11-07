50b66c128fe3bb0adafa1c6cd9e23d9c9b3a900f
xxnonameboyzz <imthebestfcktherest@gmail.com>
2018-11-07 20:09:18 +0000

    .

--- web/admin/routes/admin.js
+++ web/admin/routes/admin.js
@@ -9,7 +9,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 router.get('/', function(req, res) {
-    res.render('index');
+    res.render('admin');
 });
 
 module.exports = router;
