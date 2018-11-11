2912b71c6d33b99b7cf44b0044eedcabf23b9631
xxnonameboyzz <imthebestfcktherest@gmail.com>
2018-11-11 12:59:34 +0000

    .

--- web/app.js
+++ web/app.js
@@ -19,7 +19,6 @@ let app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(expressLayouts);
 app.use(logger('dev'));
 app.use(express.json());
