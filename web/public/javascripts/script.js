03d779b9fb71d8ec05a413eaefdf43ec4076074d
xxnonameboyzz <imthebestfcktherest@gmail.com>
2018-11-14 17:04:22 +0000

    .

--- web/public/javascripts/script.js
+++ web/public/javascripts/script.js
@@ -1,3 +1,44 @@
+let slideCount = $('#slider ul li').length;
+let slideWidth = $('#slider ul li').width();
+let slideHeight = $('#slider ul li').height();
+let sliderUlWidth = slideCount * slideWidth;
+
+$('#slider').css({ width: slideWidth, height: slideHeight });
+
+$('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+
+$('#slider ul li:last-child').prependTo('#slider ul');
+
+$('.slider').css('background', $('#slider > ul > li:nth-child(2)').css('background'));
+
+function moveLeft() {
+    $('#slider ul').animate({
+        left: + slideWidth
+    }, 600, function () {
+        $('#slider ul li:last-child').prependTo('#slider ul');
+        $('#slider ul').css('left', '');
+        $('.slider').css('background', $('#slider > ul > li:nth-child(2)').css('background'));
+    });
+};
+
+function moveRight() {
+    $('#slider ul').animate({
+        left: - slideWidth
+    }, 600, function () {
+        $('#slider ul li:first-child').appendTo('#slider ul');
+        $('#slider ul').css('left', '');
+        $('.slider').css('background', $('#slider > ul > li:nth-child(2)').css('background'));
+    });
+};
+
+$('a.control_prev').click(function () {
+    moveLeft();
+});
+
+$('a.control_next').click(function () {
+    moveRight();
+});
+
 $(window).scroll(function() {
     if ($(this).scrollTop() >= 110) {       
         $('a[href="#top"]').css('display', 'block');   
