141699751c49b151cdae857d69a46ad3a6f6f483
xxnonameboyzz <imthebestfcktherest@gmail.com>
2018-11-20 23:30:29 +0000

    .

--- python/streamPath.py
+++ python/streamPath.py
@@ -9,5 +9,7 @@ xHash = [hash]
 with open('/home/tvga/tvga/python/hash.json', 'w') as outfile:
     json.dump(xHash, outfile, indent=4)
 
+print(hash)
+
 os.system('mv /home/tvga/tvga/web/public/stream-*/ /home/tvga/tvga/web/public/stream-{}'.format(hash))
 os.system('pm2 update')
