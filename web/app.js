6cf074b571ea7a8a0c57ce3a0e7699c98054da55
xxnonameboyzz <imthebestfcktherest@gmail.com>
2018-11-20 13:24:27 +0000

    .

--- web/app.js
+++ web/app.js
@@ -18,7 +18,7 @@ let adminApp = require('./admin/app')
 let app = express();
 
 const apiLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
+    windowMs: 1 * 4 * 1000,
     max: 1,
     message:
     "Too many accounts created from this IP, please try again after an hour"
