5f71c505fe351b6fbd341644ed3f1bacd553323e
xxnonameboyzz <imthebestfcktherest@gmail.com>
2018-11-20 16:17:37 +0000

    .

--- web/routes/index.js
+++ web/routes/index.js
@@ -11,7 +11,7 @@ router.get('/:canal', function(req, res, next) {
   let grupo;
   for(grupo in canais) {if(canal.toLowerCase().includes(grupo)) break;}
   
-  res.render('stream', { title: 'TVGA > ' + canais[grupo][canal]['nome'], file: '/stream/' + canal + '.m3u8' });
+  res.render('stream', { title: 'TVGA > ' + canais[grupo][canal]['nome'], file: '/stream/' + canal });
 });
 
 module.exports = router;
