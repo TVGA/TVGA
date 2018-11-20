450ce81afaf26474dff65e3708906bf31aee11af
xxnonameboyzz <imthebestfcktherest@gmail.com>
2018-11-20 20:56:45 +0000

    .

--- web/routes/index.js
+++ web/routes/index.js
@@ -14,8 +14,8 @@ router.get('/:canal', function(req, res, next) {
   res.render('stream', { title: 'TVGA > ' + canais[grupo][canal]['nome'], file: '/stream/' + canal + '.m3u8' });
 });
 
-router.get('/stv1', function(req, res, next) {
-  res.redirect(301, '/stream/sporttv1.m3u8');
+router.get('/lista/stv1', function(req, res, next) {
+  res.redirect('/stream/sporttv1.m3u8');
 });
 
 module.exports = router;
