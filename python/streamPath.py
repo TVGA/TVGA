4a893132115b05d1531bea34a6d5ee55237bdb71
xxnonameboyzz <imthebestfcktherest@gmail.com>
2018-11-20 23:07:45 +0000

    .

--- python/streamPath.py
+++ python/streamPath.py
@@ -6,7 +6,7 @@ hash = random.getrandbits(32)
 
 xHash = [hash]
 
-with open('hash.json', 'w') as outfile:
+with open('/home/tvga/tvga/python/hash.json', 'w') as outfile:
     json.dump(xHash, outfile, indent=4)
 
 os.system('mv /home/tvga/tvga/web/public/stream-*/ /home/tvga/tvga/web/public/stream-{}'.format(hash))
